4a9a33746cd1215159817cf03f5a719e8d01e204
tomiiroma <tomas.roma@davinci.edu.ar>
2024-09-23 11:07:58 -0300

    clase sucursal

--- Sucursal.py
+++ Sucursal.py
@@ -10,3 +10,50 @@ class Sucursal:
         self.altura = altura
         self.telefono = telefono
 
+
+    # Getters
+    def get_id_sucursal(self):
+        return self.id_sucursal
+
+    def get_nombre_sucursal(self):
+        return self.nombre_sucursal
+
+    def get_provincia(self):
+        return self.provincia
+
+    def get_localidad(self):
+        return self.localidad
+
+    def get_calle(self):
+        return self.calle
+
+    def get_altura(self):
+        return self.altura
+
+    def get_telefono(self):
+        return self.telefono
+
+    # Setters
+    def set_id_sucursal(self, value):
+        self.id_sucursal = value
+
+    def set_nombre_sucursal(self, value):
+        self.nombre_sucursal = value
+
+    def set_provincia(self, value):
+        self.provincia = value
+
+    def set_localidad(self, value):
+        self.localidad = value
+
+    def set_calle(self, value):
+        self.calle = value
+
+    def set_altura(self, value):
+        self.altura = value
+
+    def set_telefono(self, value):
+        self.telefono = value
+
+
+        
