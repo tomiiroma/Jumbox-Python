a5d642e0a086555a7e640503a3a742aecee4ea2c
tomiiroma <tomas.roma@davinci.edu.ar>
2024-10-28 11:15:53 -0300

    cambios

--- routes.py
+++ routes.py
@@ -5,8 +5,10 @@ from app.models.Sucursal import Sucursal
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/",  methods=["GET", "POST"])
 def hello_world():
+        
+
     sucursales = [
             Sucursal(1, "Sucursal Centro", "Buenos Aires", "Capital", "Calle 1", 100, "011-1234-5678"),
             Sucursal(2, "Sucursal Norte", "Buenos Aires", "San Martín", "Calle 2", 200, "011-2345-6789"),
@@ -14,19 +16,19 @@ def hello_world():
             Sucursal(4, "Sucursal Oeste", "Buenos Aires", "Morón", "Calle 4", 400, "011-4567-8901"),
             Sucursal(5, "Sucursal Este", "Buenos Aires", "La Plata", "Calle 5", 500, "011-5678-9012"),
         ]
-    return render_template("index.html", sucursales=sucursales)
+    
+    message = ""
+    if request.method == "POST":
+        message = "Has iniciado sesión"
+
+    return render_template("index.html", sucursales=sucursales, message=message)
 
 
     
-@app.route("/formulario")
+@app.route("/login")
 def formulario():
     return render_template('login.html')
 
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        return render_template("success.html")
     
 
 if __name__ == "__main__":
