a775263f8716c09ceb0a53475416a6a51d0dc185
Tomas Roma <tomas.roma@davinci.edu.ar>
2024-10-29 00:14:14 -0300

    probando insert de usuario

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from db.base_datos import inicializar_bd
+from db.conexion import inicializar_bd
 from controller.usuario_controlador import insertar_usuario
 
 if __name__ == '__main__':
     inicializar_bd()  # Inicializa la base de datos y crea las tablas
 
     # Ejemplo de insertar un usuario
-    insertar_usuario(1, "Dani", "dani@davinci.com", "contraseña123", "admin")
+    insertar_usuario(1, "Dani", "dani@davinci.edu.araaa", "contraseña123", "admin")
