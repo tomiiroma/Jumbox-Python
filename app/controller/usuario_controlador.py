e15772603a3ab1e27aefeea5aa4af96c6cb7a46f
Agustin Aguero <agustin.aguero@davinci.edu.ar>
2024-10-29 20:33:31 -0300

    Login y casi Logout

--- app/controller/usuario_controlador.py
+++ app/controller/usuario_controlador.py
@@ -1,11 +1,27 @@
-from conexion import obtener_conexion
+from app.conexion import obtener_conexion
 
 conn = obtener_conexion()
 cursor = conn.cursor()
 
 def insertar_usuario(fk_sucursal,nombre,email, contraseña,rol):    
 
+    conn = obtener_conexion()
+    cursor = conn.cursor()
+
     cursor.execute("INSERT INTO usuario (fk_sucursal, nombre, email, contraseña, rol) VALUES (?, ?, ?, ?, ?)",(fk_sucursal, nombre, email, contraseña, rol))    
     conn.commit()
 
-insertar_usuario(1,"jose", "jsoaaae@gmail.com", "contraseña", "usuario")
+
+def verificar_login(email, cont):
+    conn = obtener_conexion()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM USUARIO WHERE email = ? AND contraseña = ?", (email, cont))
+    user = cursor.fetchone()
+
+    conn.close()
+
+    if user:
+        return user
+    else:
+        return None
--- routes.py
+++ routes.py
@@ -1,20 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, flash
 from app.models.Sucursal import Sucursal
 import app.conexion as db
 from app.controller.sucursal_controlador import get_sucursal 
+from app.controller.usuario_controlador import verificar_login
 
 app = Flask(__name__)
+app.secret_key = "secretkey"
 
 
 @app.route("/",  methods=["GET", "POST"])
 def hello_world():
     db.iniciar_db()
-    message = ""
+    success = "Inicio de Sesion Exitoso"
+    error="Email o contraseña incorrectas"
     sucursales = get_sucursal()
-    if request.method == "POST":
-        message = "Has iniciado sesión"
 
-    return render_template("index.html", message=message, sucursales=sucursales)
+    if request.method == "POST":
+        email = request.form["email"]
+        cont = request.form["cont"]
+
+        if verificar_login(email, cont):
+            session['usuario'] = email
+            return render_template("index.html", success=success, sucursales=sucursales)
+        else:
+            return render_template('login.html', error=error)
+
+        
+    return render_template("index.html", sucursales=sucursales)
 
     
     
@@ -22,6 +34,9 @@ def hello_world():
 def formulario():
     return render_template('login.html')
 
+@app.route("/logout")
+def logout():
+    return "cerrar sesion"
     
 
 if __name__ == "__main__":
