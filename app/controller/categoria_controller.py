cea6f924421eba77ceb3ee647bc82759cdd3c536
att124 <daniel.fernandez@davinci.edu.ar>
2024-10-30 15:45:17 -0300

    lista de categorias sin css

--- app/controller/categoria_controller.py
+++ app/controller/categoria_controller.py
@@ -1,5 +1,5 @@
-from ..db.conexion import obtener_conexion
-
+from app.conexion import obtener_conexion
+from ..models.Categoria import Categoria
 
 def agregar_categoria(nombre,estado):
     
@@ -22,6 +22,39 @@ def agregar_categoria(nombre,estado):
     except Exception as e:
         print(f"Ocurrió un error: {e}")  # Manejo de cualquier otro error
     finally:
+        cursor.close()
         conn.close()
 
+
+def mostrar_categorias():
+
+    conn = obtener_conexion()
+    cursor = conn.cursor()
+
+    try:
+        
+        lista_categorias = []
+
+        cursor.execute("SELECT * FROM categoria")
+
+        filas = cursor.fetchall()
+
+        for columna in filas:
+
+            categoria = Categoria(id_categoria=columna[0], nombre=columna[1], visible=columna[2])
+            lista_categorias.append(categoria)
+
+        if len(filas) != 0:
+            return lista_categorias
+        else:
+            return []
+      
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")  
+
+        
+
+    finally:
+        cursor.close()
+        conn.close()
 
--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, flash
 from app.models.Sucursal import Sucursal
-
-from app.controller.categoria_controller import agregar_categoria
+from app.models.Categoria import Categoria
+from app.controller.categoria_controller import agregar_categoria,mostrar_categorias
 import app.conexion as db
 from app.controller.sucursal_controlador import get_sucursal 
 from app.controller.usuario_controlador import verificar_login
@@ -43,7 +43,7 @@ def nueva_categoria():
         nombre = request.form['nombre']
         estado = 1
 
-        datos = agregar_categoria(nombre, estado)
+        agregar_categoria(nombre, estado)
 
         mensaje = "Categoria agregada correctamente."
 
@@ -54,6 +54,17 @@ def nueva_categoria():
 @app.route("/logout")
 def logout():
     return "cerrar sesion"
+
+
+@app.route("/categoria/index")
+def index_categoria():
+
+
+    categorias = mostrar_categorias()
+
+    print(categorias)
+
+    return render_template('categoria/index.html', categorias=categorias)
 
 
 if __name__ == "__main__":
