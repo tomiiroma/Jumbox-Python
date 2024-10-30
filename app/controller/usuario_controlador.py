f3ad22133c4492ca55ae81654829a3791ed39e19
att124 <daniel.fernandez@davinci.edu.ar>
2024-10-30 10:50:22 -0300

    merge con Agus

--- app/controller/usuario_controlador.py
+++ app/controller/usuario_controlador.py
@@ -1,32 +1,4 @@
-<<<<<<< HEAD
-from db.conexion import obtener_conexion
-import sqlite3
 
-def insertar_usuario(fk_sucursal, nombre, email, contraseña, rol):
-    conn = obtener_conexion()
-    cursor = conn.cursor()
-    
-    try:
-        # Verifica si el correo ya existe
-        cursor.execute("SELECT * FROM usuario WHERE email = ?", (email,))
-        if cursor.fetchone():  # Si se encuentra un registro
-            print("Error: El email ya está registrado.")
-            return  # Sale de la función sin hacer el insert
-        
-        # Procede a insertar el nuevo usuario
-        cursor.execute(
-            "INSERT INTO usuario (fk_sucursal, nombre, email, contraseña, rol) VALUES (?, ?, ?, ?, ?)",
-            (fk_sucursal, nombre, email, contraseña, rol)
-        )
-        conn.commit()
-        print("Usuario insertado con éxito.")
-    except sqlite3.IntegrityError:
-        print("Error: El email ya está registrado.")  # Se imprimirá si aún se da un error
-    except Exception as e:
-        print(f"Ocurrió un error: {e}")  # Manejo de cualquier otro error
-    finally:
-        conn.close()
-=======
 from app.conexion import obtener_conexion
 
 conn = obtener_conexion()
@@ -54,4 +26,4 @@ def verificar_login(email, cont):
         return user
     else:
         return None
->>>>>>> origin/agus
+
